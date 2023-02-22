12a326c3c1155d9c54083e2a35b3f0a823a4011e
Robin Jin <peace1019@126.com>
2023-02-22 23:02:32 +0800

    prepare header

--- checkproc.py
+++ checkproc.py
@@ -31,7 +31,7 @@ def check_proxy_ip(proxy_address, check_url, header_info):
         info_logger(current_ip, '\033[41m')
 
 
-def check_proc(proxy_list_file, proxy_success_list, check_url, header_referer, header_host):
+def check_proc(proxy_list_file, proxy_success_list, check_url, header_referer = '', header_host = ''):
     # show information
     info_logger('=====================================================')
     info_logger('Check process start with following parameters')
@@ -43,5 +43,13 @@ def check_proc(proxy_list_file, proxy_success_list, check_url, header_referer, h
     info_logger('Header Host' + header_host)
     info_logger('=====================================================')
 
+    # prepare request header
+    header_info = requests.utils.default_headers()
+    header_info['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.46'
+    if ((header_referer != '') and (len(header_referer) > 0)):
+        header_info['Referer'] = header_referer
+    if ((header_host != '') and (len(header_host) > 0)):
+        header_info['Host'] = header_host
+
     # check proxy
-    check_proxy_ip('socks5://158.69.225.110:59166', check_url, header_referer)
+    check_proxy_ip('socks5://158.69.225.110:59166', check_url, header_info)
